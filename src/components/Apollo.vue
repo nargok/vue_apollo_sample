ce676be373f0636615025deee2d53ac3b8df24a2
nargok <nargok@gmail.com>
2019-03-24 15:43:17 +0900

    画面で入力した値でQueryの結果表示数を変える処理を実装

--- src/components/Apollo.vue
+++ src/components/Apollo.vue
@@ -5,9 +5,14 @@
   >
     <template slot-scope="{ result: { loading, error, data }}">
       <span v-if="loading">Loading...</span>
-      <span v-else-if="error">An error occured</span>
+      <span v-else-if="error">An error occured {{error.message}}}</span>
       <section v-else-if="data">
-        <div>My Apollo Component</div>
+        <h2>表示数を変更する</h2>
+        <div>
+          <input type="number" class="per_page" v-model="input_per_page"/>
+          <button @click="changeDisplayNumberOfItems">表示</button>
+        </div>
+        <hr>
         <ul v-for="item in data.allTags" class="tag-list">
           <li class="tag-item">{{item.name}}</li>
         </ul>
@@ -25,9 +30,15 @@
     data() {
       return {
         query: GET_ALL_TAGS,
-        tag_per_page: 15
+        tag_per_page: 15,
+        input_per_page: 0,
       };
     },
+    methods: {
+      changeDisplayNumberOfItems() {
+        this.tag_per_page = parseInt(this.input_per_page);
+      },
+    }
   }
 </script>
 
